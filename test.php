8220f9cbce70b30b9974d6085675b5fc0f6fe70c
Joey585 <joey585@joey585.com>
2021-07-08 03:40:52 -0700

    Testing with php
    v0.054
    
    - Joey585 :)

--- test.php
+++ test.php
@@ -9,13 +9,13 @@
 $headers = [ 'Content-Type: application/json; charset=utf-8' ];
 $POST = [ 'username' => 'Testing BOT', 'content' => $ip ];
 
-$ch = curl_init();
+/* $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($POST));
-$response   = curl_exec($ch);
+$response   = curl_exec($ch); */
 
 ?>
